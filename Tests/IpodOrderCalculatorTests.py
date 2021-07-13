31b6097464cfd06409957e8e0aabd7085568cb30
Srutarshi-Ghosh <srutarshighosh06@gmail.com>
2021-07-13 11:39:21 +0530

    Updated Input Order Calculator

---
+++ Tests/IpodOrderCalculatorTests.py
@@ -0,0 +1,11 @@
+import unittest
+from models.IpodOrderCalculator import IpodOrderCalculator
+
+class MyTestCase(unittest.TestCase):
+
+    def test_(self):
+        self.assertEqual(True, False)
+
+
+if __name__ == '__main__':
+    unittest.main()
--- models/IpodOrderCalculator.py
+++ models/IpodOrderCalculator.py
@@ -26,31 +26,33 @@ class IpodOrderCalculator:
     def _check_valid_order_size(ordered_ipods):
         return ordered_ipods < 10 or ordered_ipods % 10 == 0
 
-    def calculate_shipping_country_cost(self, shipping_country, shipped_ipods):
+    def _calculate_shipping_country_cost(self, shipping_country, shipped_ipods):
         return shipping_country.calculate_order_cost(shipped_ipods) + (self.shipping_cost_per_ten_units * (shipped_ipods // 10))
 
     @staticmethod
-    def calculate_ordering_country_cost(ordering_country, shipped_ipods):
+    def _calculate_ordering_country_cost(ordering_country, shipped_ipods):
         return ordering_country.calculate_order_cost(shipped_ipods)
 
     def _calculate_minimum_cost_of_ordering(self, ordering_country, shipping_country, ordered_ipods):
         if ordered_ipods > self.ipods_in_stock_per_country:
             extra_ipods = ordered_ipods - self.ipods_in_stock_per_country
-            order_price = min(self.calculate_ordering_country_cost(ordering_country, self.ipods_in_stock_per_country) + self.calculate_shipping_country_cost(shipping_country, extra_ipods),
-                              self.calculate_ordering_country_cost(ordering_country, extra_ipods) + self.calculate_shipping_country_cost(shipping_country, self.ipods_in_stock_per_country))
+            order_price = min(self._calculate_ordering_country_cost(ordering_country, self.ipods_in_stock_per_country) + self._calculate_shipping_country_cost(shipping_country, extra_ipods),
+                              self._calculate_ordering_country_cost(ordering_country, extra_ipods) + self._calculate_shipping_country_cost(shipping_country, self.ipods_in_stock_per_country))
             return order_price
         
-        return min(self.calculate_ordering_country_cost(ordering_country, ordered_ipods), self.calculate_shipping_country_cost(shipping_country, ordered_ipods))
-    
+        return min(self._calculate_ordering_country_cost(ordering_country, ordered_ipods), self._calculate_shipping_country_cost(shipping_country, ordered_ipods))
+
+    def _check_country_stock(self, ordered_ipods):
+        return ordered_ipods < (self.argentina.ipods_in_stock + self.brazil.ipods_in_stock)
+
     def get_order_cost(self, ordering_country_name, ordered_ipods):
         if not self._check_valid_order_size(ordered_ipods):
             raise InvalidOrderSizeException
 
-        ordering_country, shipping_country = self._get_ordering_and_shipping_country(ordering_country_name)
-        
-        if ordered_ipods > ordering_country.ipods_in_stock + shipping_country.ipods_in_stock:
+        if not self._check_country_stock(ordered_ipods):
             raise OutOfStockException
-        
+
+        ordering_country, shipping_country = self._get_ordering_and_shipping_country(ordering_country_name)
         return self._calculate_minimum_cost_of_ordering(ordering_country, shipping_country, ordered_ipods)
         
 
