21c2d364d1a086e6b426b9c8fbc310c06f7c58ab
Srutarshi-Ghosh <srutarshighosh06@gmail.com>
2021-07-13 11:30:03 +0530

    Added Country Tests

--- Tests/CountryTest.py
+++ Tests/CountryTest.py
@@ -1,10 +1,19 @@
 import unittest
+from models.Country import Country
+from Exceptions.OrderedIpodsGreaterThanIpodsInStockException import OrderedIpodsGreaterThanIpodsInStockException
 
 
-class MyTestCase(unittest.TestCase):
-    def test_country_should_return(self):
-        self.assertEqual(True, False)
+class CountryTest(unittest.TestCase):
+    def test_should_return_order_cost_when_valid_order_is_placed(self):
+        argentina = Country('Argentina', 50, 100)
+        number_of_ipods = 50
+        correct_output = 50 * number_of_ipods
+        self.assertEqual(correct_output, argentina.calculate_order_cost(number_of_ipods))
 
+    def test_should_throw_exception_when_invalid_order_is_placed(self):
+        argentina = Country('Argentina', 50, 100)
+        number_of_ipods = 120
+        self.assertRaises(OrderedIpodsGreaterThanIpodsInStockException, argentina.calculate_order_cost, number_of_ipods)
 
 if __name__ == '__main__':
     unittest.main()
